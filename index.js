33f5fa8f7d72f872aed7af83e0d90b9901a0c272
neville777 <neville.james@student.moringaschool.com>
2023-09-18 09:06:54 +0300

    code-along

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+f// index.js
+
+// Function to generate thank you messages for each name
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a specified number and log each number
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
